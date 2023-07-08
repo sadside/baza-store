feac7b24523dc6f5fcdff6c2936ebbb9017c1b6a
sadside <sadside1319@gmail.com>
2023-07-09 00:54:44 +0500

    deploy

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -12,7 +12,7 @@ export default function ({ product }: IProductPageProps) {
 
 export const getStaticPaths = async () => {
   try {
-    const { data } = await axios.get(`${process.env.SERVER_URL}/api/products`);
+    const { data } = await axios.get(`${process.env.SERVER_URL}api/products`);
 
     const paths = data.map((product: IProduct) => ({
       params: { id: product.id.toString() },
