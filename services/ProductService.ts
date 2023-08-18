a677a0091278af07e13dc7a8d4f478337439e59b
sadside <sadside1319@gmail.com>
2023-08-18 12:53:01 +0500

    e

--- services/ProductService.ts
+++ services/ProductService.ts
@@ -3,7 +3,9 @@ import { IFullProduct } from "@/models/Product";
 
 export default class ProductService {
   static async getProductById(id: string) {
-    return await fetch(`${API_URL}products/product/${id}/`)
+    return await fetch(`${API_URL}products/product/${id}/`, {
+      cache: 'no-store'
+    })
 
   }
 }
