78ee5465ead8adbd9db02be5f8f4bb07fb9eb794
sadside <sadside1319@gmail.com>
2023-08-24 18:12:07 +0500

    123

--- components/products/Products.tsx
+++ components/products/Products.tsx
@@ -6,20 +6,21 @@ import { IProduct } from "@/models/Product";
 const Products = ({ products }: { products: IProduct[] }) => {
   return (
     <div className={styles.productsWrapper}>
-      {products.map((product) => (
-        <ProductItem
-          product_id={product.product_id}
-          price={product.price}
-          name={product.name}
-          colors_count={product.colors_count}
-          images={product.images}
-          key={product.modification_id}
-          modification_id={product.modification_id}
-          slug={product.slug}
-          path={product.path}
-          old_price={product.old_price}
-        />
-      ))}
+      {products?.length &&
+        products.map((product) => (
+          <ProductItem
+            product_id={product.product_id}
+            price={product.price}
+            name={product.name}
+            colors_count={product.colors_count}
+            images={product.images}
+            key={product.modification_id}
+            modification_id={product.modification_id}
+            slug={product.slug}
+            path={product.path}
+            old_price={product.old_price}
+          />
+        ))}
     </div>
   );
 };
