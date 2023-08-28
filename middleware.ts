0189d6f57879afec48851e5a7a38cfb4ba133029
sadside <sadside1319@gmail.com>
2023-08-29 00:35:43 +0700

    123

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function middleware(request: NextRequest) {
-
-
-  if (request.nextUrl.pathname.startsWith('/lk')) {
-    return NextResponse.redirect(new URL('/auth', request.url))
-  }
-}
