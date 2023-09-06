cbff6b5a7925d5e81f56f602ab901032d888418e
sadside <sadside1319@gmail.com>
2023-09-06 16:44:54 +0700

    123

--- components/screens/mainPage/MainPage.tsx
+++ components/screens/mainPage/MainPage.tsx
@@ -3,7 +3,11 @@
 import Products from "@/components/products/Products";
 import {Video} from "@/components/video/Video";
 import {IProduct} from "@/models/Product";
+import { BrowserView, MobileView } from "react-device-detect";
 import Photo from "@/components/ui/photo/Photo";
+
+
+
 interface Props {
     products: IProduct[];
 }
@@ -11,7 +15,9 @@ interface Props {
 const MainPage = ({products}: Props) => {
     return (
         <div>
-            <Video src={"./video.mp4"}/>
+            <BrowserView>
+                <Video src={"./video.mp4"}/>
+            </BrowserView>
             <Photo />
             <Products products={products}/>
         </div>
