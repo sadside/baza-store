e48c499d5e58066ab49d5dbd8d5e8270b3be4e4a
sadside <sadside1319@gmail.com>
2024-05-05 23:57:31 +0700

    3333

--- source/shared/api/http/axios-instance.ts
+++ source/shared/api/http/axios-instance.ts
@@ -2,8 +2,8 @@ import axios, { AxiosError, CreateAxiosDefaults } from 'axios';
 import { getAccessToken, getContentType, removeFromStorage } from '@shared/api/lib/api.helpers';
 import apiService from '@shared/api/api/api.service';
 
-export const API_URL_CLIENT = 'http://thebaza.ru/api/';
-export const API_URL = 'http://127.0.0.1:8000/api/';
+export const API_URL_CLIENT = 'https://thebaza.ru/api/';
+export const API_URL = 'http://127.0.0.1/api/';
 
 // export const API_URL_CLIENT = 'http://127.0.0.1:8000/api/';
 // export const API_URL = 'http://127.0.0.1:8000/api/';
