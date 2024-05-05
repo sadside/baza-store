82bea976dbed63d2ccad3acffa4d93a9761c6f67
sadside <sadside1319@gmail.com>
2024-05-06 02:56:29 +0700

    ssss

--- source/features/actual-order-actions/ui/ActualOrderActions.tsx
+++ source/features/actual-order-actions/ui/ActualOrderActions.tsx
@@ -18,35 +18,31 @@ export const ActualOrderActions = ({ order }: ActualOrderProps) => {
     cancelOrderFx(order.id);
   };
 
-  console.log(order.status);
-
   const orderIsCanceling = useUnit(cancelOrderFx.pending);
 
   return (
     <div className="uppercase flex gap-[15px] min-w-[340px]  w-[40%] text-[12px] mt-7 items-center">
       {/*{order.receiving === ReceivingEnum.cdek && <Button.Primary>Отследить посылку</Button.Primary>}*/}
-      {order.status === 'created' ||
-        (order.status === 'failed_payment' && (
-          <Button.Primary
-            className="w-[244px]"
-            onClick={() => createPaymentFx(order.id)}
-            disabled={loading}
-            loading={loading}
-          >
-            Оплатить
-          </Button.Primary>
-        ))}
-      {order.status === 'created' ||
-        (order.status === 'failed_payment' && (
-          <Button.Secondary
-            className="w-[244px]"
-            onClick={handleCancelClick}
-            loading={orderIsCanceling}
-            disabled={orderIsCanceling}
-          >
-            Отменить
-          </Button.Secondary>
-        ))}
+      {(order.status === 'created' || order.status === 'failed_payment') && (
+        <Button.Primary
+          className="w-[244px]"
+          onClick={() => createPaymentFx(order.id)}
+          disabled={loading}
+          loading={loading}
+        >
+          Оплатить
+        </Button.Primary>
+      )}
+      {(order.status === 'created' || order.status === 'failed_payment') && (
+        <Button.Secondary
+          className="w-[244px]"
+          onClick={handleCancelClick}
+          loading={orderIsCanceling}
+          disabled={orderIsCanceling}
+        >
+          Отменить
+        </Button.Secondary>
+      )}
       <button
         className="text-[12px] uppercase text-black-300 flex justify-center items-center py-2.5 font-medium"
         onClick={() => contactsModalChanged(true)}
