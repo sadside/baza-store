7ecccce85873483d92460314cf34c86665ba63e1
sadside <sadside1319@gmail.com>
2024-05-14 20:43:30 +0700

    222

--- source/widgets/type-of-obtain-address/ui/TypeOfObtainAddress.tsx
+++ source/widgets/type-of-obtain-address/ui/TypeOfObtainAddress.tsx
@@ -1,34 +1,19 @@
-"use client";
-import React from "react";
-import { Select } from "@shared/ui/select";
-import { useUnit } from "effector-react";
-import { $activeTypeAddressesSelf, changeTypeAddresses } from "@/stores/addresses/init";
+'use client';
+import React from 'react';
 
 export const TypeOfObtainAddress = () => {
-  const value = useUnit($activeTypeAddressesSelf);
-  const options = [
-    {
-      name: "в пункте сдэк",
-      value: "cdek"
-    },
-    {
-      name: "Курьером",
-      value: "personal"
-    }
-  ];
-  // @ts-ignore
   return (
     <div>
       <div className="mt-5">Способ получения</div>
       <div className="w-[463.63px] max-[470px]:w-full">
         {
           // @ts-ignore
-          <Select
-            onChange={(e: any) => changeTypeAddresses(e.target.value)}
-            value={value}
-            color={"white"}
-            options={options}
-          />
+          // <Select
+          //   onChange={(e: any) => changeTypeAddresses(e.target.value)}
+          //   value={value}
+          //   color={"white"}
+          //   options={options}
+          // />
         }
       </div>
     </div>
